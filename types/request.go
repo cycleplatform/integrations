1d6f60750f537280a98b170ee90ad1540713f54b
Jake Warner <jwarner@petrichor.io>
2021-07-20 14:09:38 -0700

    add request wrapper

--- types/request.go
+++ types/request.go
@@ -1,6 +1,12 @@
 package types
 
+import "encoding/json"
+
 type RequestWrapper struct {
-	Auth    *Auth       `json:"auth"`
-	Request interface{} `json:"request"`
+	Auth    *Auth           `json:"auth"`
+	Request json.RawMessage `json:"request"`
+}
+
+func (rw RequestWrapper) Unmarshal(out interface{}) error {
+	return json.Unmarshal(rw.Request, out)
 }
