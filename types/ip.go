5aa9d52f11d2c5d516e880098876af93d876cbd1
Jake Warner <jwarner@petrichor.io>
2021-07-22 13:26:02 -0700

    more ip structs (still draft)

--- types/ip.go
+++ types/ip.go
@@ -1,32 +1,54 @@
 package types
 
+import (
+	locationObj "github.com/cycleplatform/integrations/types/objects/location"
+)
+
 type (
 	NewIPRequest struct {
-		Type     IPKind      `json:"type"`
-		Location string      `json:"location"`
-		Cycle    CycleIPMeta `json:"cycle"`
+		Type     IPKind                `json:"type"`
+		ServerID string                `json:"server_id"`
+		Location *locationObj.Location `json:"location"`
+		Cycle    CycleIPMeta           `json:"cycle"`
+	}
+
+	NewIPResponse struct {
+		IpID    string `json:"ip_id"` // what is the ID of the ip at the provider?
+		CIDR    string `bson:"cidr" json:"cidr"`
+		Gateway string `bson:"gateway" json:"gateway"`
+		Netmask string `bson:"netmask" json:"netmask"`
+		Network string `bson:"network" json:"network"`
 	}
 
 	DeleteIPRequest struct {
-		ProviderId string      `json:"provider_id"` // what is the ID of the ip at the provider?
-		Type       IPKind      `json:"type"`
-		Cycle      CycleIPMeta `json:"cycle"`
+		Type     IPKind                `json:"type"`
+		IpID     string                `json:"ip_id"` // what is the ID of the ip at the provider?
+		ServerID string                `json:"server_id"`
+		Location *locationObj.Location `json:"location"`
+		Cycle    CycleIPMeta           `json:"cycle"`
 	}
 
 	AssignIPRequest struct {
-		ProviderId           string      `json:"provider_id"`            // what is the ID of the ip at the provider?
-		ProviderAssignmentId string      `json:"provider_assignment_id"` // if the provider requires a different ID for assignments
-		Type                 IPKind      `json:"type"`
-		Location             string      `json:"location"`
-		Cycle                CycleIPMeta `json:"cycle"`
+		Type           IPKind                `json:"type"`
+		IpID           string                `json:"ip_id"`            // what is the ID of the ip at the provider?
+		IpAssignmentID string                `json:"ip_assignment_id"` // what is the ID of the ip assignment at the provider?
+		ServerID       string                `json:"server_id"`
+		Location       *locationObj.Location `json:"location"`
+		Cycle          CycleIPMeta           `json:"cycle"`
+	}
+
+	AssignIPResponse struct {
+		IpID           string `json:"ip_id"`            // what is the ID of the ip at the provider?
+		IpAssignmentID string `json:"ip_assignment_id"` // what is the ID of the ip assignment at the provider?
 	}
 
 	UnassignIPRequest struct {
-		ProviderId           string      `json:"provider_id"`            // what is the ID of the ip at the provider?
-		ProviderAssignmentId string      `json:"provider_assignment_id"` // if the provider requires a different ID for assignments
-		Type                 IPKind      `json:"type"`
-		Location             string      `json:"location"`
-		Cycle                CycleIPMeta `json:"cycle"`
+		Type           IPKind                `json:"type"`
+		IpID           string                `json:"ip_id"`            // what is the ID of the ip at the provider?
+		IpAssignmentID string                `json:"ip_assignment_id"` // what is the ID of the ip assignment at the provider?
+		ServerID       string                `json:"server_id"`
+		Location       *locationObj.Location `json:"location"`
+		Cycle          CycleIPMeta           `json:"cycle"`
 	}
 
 	CycleIPMeta struct {
