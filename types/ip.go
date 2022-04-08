e62678e06d149aec25064985fdfba9ec9b6ad948
Jake Warner <jwarner@petrichor.io>
2022-04-07 17:34:06 -0700

    add CIDR and NAT IP to ReleaseIP request

--- types/ip.go
+++ types/ip.go
@@ -30,6 +30,8 @@ type (
 		Kind                  IPKind        `json:"kind"`
 		IpId                  string        `json:"ip_id"`            // what is the ID of the ip at the provider?
 		IpAssignmentId        string        `json:"ip_assignment_id"` // what is the ID of the ip assignment at the provider?
+		CIDR                  string        `bson:"cidr" json:"cidr"`
+		NATPrivateIP          *string       `bson:"nat_private_ip" json:"nat_private_ip"`
 		ServerId              string        `json:"server_id"`
 		LocationId            string        `json:"location_id"`
 		LocationConfiguration Configuration `json:"location_configuration"`
